7ea4756b7984807c289ecde12e86a181beecc995
w0nderful <w0nderful@users.noreply.github.com>
2023-11-21 11:36:17 +0800

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -1,6 +1,17 @@
 #TODO
-
+import torch
+from torchvision import datasets, transforms
 def get_dataset(batch_size, num_workers):
     dataset = None
-    
+    test_loader = torch.utils.data.DataLoader(
+        datasets.GTSRB(
+            "./data",
+            train=False,
+            download=True,
+            transform=transforms.Compose([transforms.ToTensor(),]),
+        ),
+        batch_size=batch_size,
+        shuffle=True, 
+        num_workers=num_workers,
+    )
     return dataset 
