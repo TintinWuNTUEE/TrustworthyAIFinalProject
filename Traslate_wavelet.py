1db904316f1066d2376c829d6b3b82da33525c2a
w0nderful <10183340+w0nderful@users.noreply.github.com>
2023-11-28 14:36:41 +0800

    add get_wavelet, FGSM in dataset

--- Traslate_wavelet.py
+++ Traslate_wavelet.py
@@ -62,22 +62,54 @@ if __name__ == '__main__':
     
     titles = ['Approximation', ' Horizontal detail',
           'Vertical detail', 'Diagonal detail']
+    titles_LH = ['LL', ' HH']
     classes=40
+    train_LL = copy.deepcopy(train_loader)
+    train_HH = copy.deepcopy(train_loader)
 
     for i in range(10):
-        for batch_idx, (inputs,targets) in enumerate(test_loader):
-            if(targets == i):
-                LL, (LH, HL, HH) = pywt.dwt2(inputs[0,:,:,:],'haar')#inputs(batch,3,224,224)
-                print (torch.min(inputs[0,:,:,:]),torch.max(inputs[0,:,:,:]))
-                print (LH.min(),LH.max(),len(LH))
-                for i,a in enumerate([LL, LH, HL, HH]):  #3,114,114
-                    ax = fig.add_subplot(1, 4, i + 1)
+        #for batch_idx, (inputs,targets) in enumerate(test_loader):
+        #print("loader samples=",train_loader.dataset._samples[0])
+        #print("data:",type(train_LL.dataset._samples[0]))
+        for idx in range(0,len(train_loader.dataset._samples)):
+            #if(targets == i):
+                #LL, (LH, HL, HH) = pywt.dwt2(inputs[0,:,:,:],'haar')#inputs(batch,3,224,224)
+                
+                LL, (LH, HL, HH) = pywt.dwt2(train_LL.dataset[idx][0],'haar')#inputs(batch,3,224,224)
+                
+                # print("LL shape = ",LL.shape[0])
+                # print("LL type= ",type(LL))
+                HH_inv=pywt.idwt2((np.zeros((LL.shape)), 
+                                   (np.zeros((LH.shape)), np.zeros((HL.shape)), HH)
+                                   ),'haar')
+                LL_inv=pywt.idwt2((LL, 
+                                   (np.zeros((LH.shape)), np.zeros((HL.shape)), np.zeros((HH.shape)))
+                                   ),'haar')
+                
+
+                #print (torch.min(inputs[0,:,:,:]),torch.max(inputs[0,:,:,:]))
+                #print (LH.min(),LH.max(),len(LH))
+                for id,a in enumerate([LL, LH, HL, HH]):  #3,114,114
+                    ax = fig.add_subplot(2, 4, id + 1)
                     a=(a-a.min())/(a.max()-a.min())
                     ax.imshow(np.transpose(a,(1,2,0)), interpolation="nearest",vmin=0,vmax=1)
-                    ax.set_title(titles[i], fontsize=10)
+                    ax.set_title(titles[id], fontsize=10)
                     ax.set_xticks([])
                     ax.set_yticks([])
-
+                for id,a in enumerate([LL_inv,HH_inv]):
+                    ax = fig.add_subplot(2, 4, id + 1 + 4)
+                    
+                    a=(a-a.min())/(a.max()-a.min())
+                    ax.imshow(np.transpose(a,(1,2,0)), interpolation="nearest",vmin=0,vmax=1)
+                    ax.set_title(titles_LH[id], fontsize=10)
+                    ax.set_xticks([])
+                    ax.set_yticks([])
+                ax = fig.add_subplot(2, 4, id + 1 + 4)
+                print (HH.min(),HH.max(),len(HH))
+                ax.imshow(np.transpose(HH,(1,2,0)), interpolation="nearest",vmin=0,vmax=1)
+                ax.set_title(titles[id], fontsize=10)
+                ax.set_xticks([])
+                ax.set_yticks([])
                 fig.tight_layout()
                 plt.show()
 
@@ -94,12 +126,12 @@ if __name__ == '__main__':
     save_path = os.path.join(pathToFigure, f'wavletData.jpg')
     plt.savefig(save_path)
     fig_ori=plt.figure(2)
-    save_path = os.path.join(pathToFigure, f'OriData.jpg')
-    a=np.transpose(inputs[0,:,:,:].detach().numpy(),(1,2,0))
-    ax_ori=fig_ori.add_subplot(1,1,1)
-    ax_ori.imshow(a)
-    ax_ori.set_title('Ori_data')
-    ax_ori.set_xticks([])
-    ax_ori.set_yticks([])
-    plt.savefig(save_path)
+    # save_path = os.path.join(pathToFigure, f'OriData.jpg')
+    # a=np.transpose(inputs[0,:,:,:].detach().numpy(),(1,2,0))
+    # ax_ori=fig_ori.add_subplot(1,1,1)
+    # ax_ori.imshow(a)
+    # ax_ori.set_title('Ori_data')
+    # ax_ori.set_xticks([])
+    # ax_ori.set_yticks([])
+    # plt.savefig(save_path)
     
