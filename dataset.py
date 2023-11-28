ab6c90e9841437debf98a20aaea57f59dca144c8
w0nderful <10183340+w0nderful@users.noreply.github.com>
2023-11-28 15:51:39 +0800

    modify get_wavelet

--- dataset.py
+++ dataset.py
@@ -37,12 +37,10 @@ def get_dataset(batch_size, num_workers):
                 
     return train_loader,test_loader
 
-def get_wavelet(data_loader):
-    dataLL_loader = copy.deepcopy(data_loader)
-    dataHH_loader = copy.deepcopy(data_loader)
-    for idx in range(0,len(data_loader.dataset._samples)):
-            
-        LL, (LH, HL, HH) = pywt.dwt2(dataLL_loader.dataset[idx][0],'haar')#inputs(batch,3,224,224)
+ 
+def get_wavelet(image):
+    
+        LL, (LH, HL, HH) = pywt.dwt2(image,'haar')#inputs(batch,3,224,224)
         # print (torch.min(inputs[0,:,:,:]),torch.max(inputs[0,:,:,:]))
         # print (LH.min(),LH.max(),len(LH))
         # for i,a in enumerate([LL, LH, HL, HH]):  #3,114,114
@@ -62,7 +60,6 @@ def get_wavelet(data_loader):
 
 
     return dataLL_loader,dataHH_loader
-
 def FGSM (image, eps_v,data_grad):
     n=eps_v*data_grad.sign()
     # print(SignGrad.type())
@@ -72,3 +69,28 @@ def FGSM (image, eps_v,data_grad):
     attack_i = image + n
     attack_i = torch.clamp(attack_i,0,1) #limit the value between 0 and 1
     return attack_i
+
+#def get_wavelet(data_loader):
+#     dataLL_loader = copy.deepcopy(data_loader)
+#     dataHH_loader = copy.deepcopy(data_loader)
+#     for idx in range(0,len(data_loader.dataset._samples)): 
+#         LL, (LH, HL, HH) = pywt.dwt2(dataLL_loader.dataset[idx][0],'haar')#inputs(batch,3,224,224)
+#         # print (torch.min(inputs[0,:,:,:]),torch.max(inputs[0,:,:,:]))
+#         # print (LH.min(),LH.max(),len(LH))
+#         # for i,a in enumerate([LL, LH, HL, HH]):  #3,114,114
+#         #     ax = fig.add_subplot(1, 4, i + 1)
+#             #  a=(a-a.min())/(a.max()-a.min())
+#         #     ax.imshow(np.transpose(a,(1,2,0)), interpolation="nearest",vmin=0,vmax=1)
+#         #     ax.set_title(titles[i], fontsize=10)
+#         #     ax.set_xticks([])
+#         #     ax.set_yticks([])
+#         # fig.tight_layout()
+#         # plt.show()
+#         LL = (LL-LL.min())/(LL.max()-LL.min())      #range[0,1]
+#         HH = (HH-HH.min())/(HH.max()-HH.min())      #range[0,1]
+#         dataLL_loader.dataset[idx][0] = LL
+#         dataHH_loader.dataset[idx][0] = HH
+        
+
+
+#     return dataLL_loader,dataHH_loader
