684c55f9b0c87d5231ecc4e60ca2e30a49d1ffcf
w0nderful <10183340+w0nderful@users.noreply.github.com>
2023-11-30 16:08:35 +0800

    Resnet FGSM Attack done

--- optimizer.py
+++ optimizer.py
@@ -3,6 +3,7 @@ import torch.optim.lr_scheduler as lr_scheduler
 def get_optimizer(name, model, lr, weight_decay):
     if name == 'Adam':
         optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+        # optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=lr, weight_decay=weight_decay)
         scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
     else:
         raise NotImplementedError(f"Optimizer '{name}' is not implemented.")
